d286c96defcf8cdaba34d1b5a37c63a058b15f0d
jduthon <duthon.jean@gmail.com>
2018-02-09 21:53:25 +0100

    Better array sum with init value 0

--- src/utils/array.js
+++ src/utils/array.js
@@ -1,3 +1,3 @@
-export const sum = arr => arr.reduce((sum, v) => sum + v);
+export const sum = arr => arr.reduce((sum, v) => sum + v, 0);
 export const last = arr => arr[arr.length - 1];
 export const flatten = arr => arr.reduce((flat, v) => [...flat, ...v], []);
