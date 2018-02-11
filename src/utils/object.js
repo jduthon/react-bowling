81b7417da98ab0b2c35d665d5cedb67c228f3091
jduthon <duthon.jean@gmail.com>
2018-02-11 09:38:21 +0100

    Fix omitKeys

--- src/utils/object.js
+++ src/utils/object.js
@@ -1,7 +1,10 @@
 export const omitKeys = (obj, keys) =>
   Object.keys(obj)
-    .filter(k => keys.indexOf(k) !== -1)
-    .reduce((acc, k) => ({
-      ...acc,
-      [k]: obj[k],
-    }));
+    .filter(k => keys.indexOf(k) === -1)
+    .reduce(
+      (acc, k) => ({
+        ...acc,
+        [k]: obj[k],
+      }),
+      {}
+    );
